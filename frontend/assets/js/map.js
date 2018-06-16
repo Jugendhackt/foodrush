553a6aaa9f92ed67bf17bb999446590204dd5f4b
elisabethschiele <elisabethschiele@gmail.com>
2018-06-16 17:51:18 +0200

    funktion markers

--- frontend/assets/js/map.js
+++ frontend/assets/js/map.js
@@ -1,4 +1,5 @@
 var mymap = L.map('mapid').setView([50.110, 8.682], 13);50.110
+var markers=[];
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -6,3 +7,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
    id: 'mapbox.streets',
    accessToken: 'pk.eyJ1IjoiZWxpc2FiZXRoc2NoaWVsZSIsImEiOiJjamloZHZxZDAxYnU5M3FsaDBkNm12ejlvIn0.ccE1-K_ZT2BOId24hO4BvQ'
 }).addTo(mymap);
+
+
+function addMarker(location){
+  var marker = L.marker([location.long,location.lat]).addTo(mymap);
+  markers.push(marker);
+  }
