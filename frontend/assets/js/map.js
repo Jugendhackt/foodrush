bfe590d8fc20142ccb225f290f74f08c7e660b3f
elisabethschiele <elisabethschiele@gmail.com>
2018-06-16 15:46:58 +0200

    home 1.0

---
+++ frontend/assets/js/map.js
@@ -0,0 +1,8 @@
+var mymap = L.map('mapid').setView([50.110, 8.682], 13);50.110
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   maxZoom: 18,
+   id: 'mapbox.streets',
+   accessToken: 'pk.eyJ1IjoiZWxpc2FiZXRoc2NoaWVsZSIsImEiOiJjamloZHZxZDAxYnU5M3FsaDBkNm12ejlvIn0.ccE1-K_ZT2BOId24hO4BvQ'
+}).addTo(mymap);
