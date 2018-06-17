7cfdb44dc07abe069e84266a96a72112b2ec481e
flawnn <baran.peters@lessing-ffm.net>
2018-06-17 02:38:55 +0200

    LOL

--- frontend/assets/js/core/main.js
+++ frontend/assets/js/core/main.js
@@ -1,7 +1,44 @@
+
+var url_string = window.location.href;
+var url = new URL(url_string);
+var id = url.searchParams.get("id");
+console.log(id);
+
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
-    }
+      offerJson: [],
+      foodJson: []
+    },
+    mounted () {
+      axios
+        .get('http://localhost:3000/bestellungen/' + id, {}, {
+          headers: {
+              'Content-Type': 'application/json',
+          }
+      })
+        .then(response => (this.offerJson = response.data));
+        axios
+          .get('http://localhost:3000/artikel/5b255307dd165d57b707a769', {}, {
+        headers: {
+            'Content-Type': 'application/json',
+          }
+      })
+          .then(response => (this.foodJson = response.data))
+    },
+    computed: {
+      getPrice () {
+        return this.offerJson["preis"]
+      },
+      getNormalPrice () {
+        return this.foodJson["preis"]
+      }
+    },
   })
+ 
+
+
+
+
+
   $(window).on('load', function(){ $("body").show();});
